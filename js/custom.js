e97926a57b5a15bfdb4646cfb6007a3ba0048583
AHpebble <adam.hogg@mypebble.co.uk>
2016-07-19 11:39:34 +0100

    updated prototype

--- js/custom.js
+++ js/custom.js
@@ -823,4 +823,37 @@ $('.modal').one('show.bs.modal', function () {
     $(this).find('.text-collapse').readmore(opts);
 });
 
+//clock
+var month =['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+
+function clock(){
+    // setting up my variables
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var day = h<11 ? 'AM': 'PM';
+    var daytoday = today.getDay();
+    var date = today.getDate();
+    var mon = today.getMonth();
+    var year = today.getFullYear();
+
+    // adding leading zeros to them
+    h = h<10? '0'+h: h;
+    m = m<10? '0'+m: m;
+    s = s<10? '0'+s: s;
+
+    // writing it down in the document
+    document.getElementById('hours').innerHTML = h;
+    document.getElementById('min').innerHTML = m;
+    document.getElementById('sec').innerHTML = s;
+    document.getElementById('time').innerHTML = day;
+    document.getElementById('day').innerHTML = date;
+    document.getElementById('month').innerHTML = month[mon];
+    document.getElementById('year').innerHTML = year;
+
+}
+var inter = setInterval(clock,400);
+
+
 }(jQuery);
